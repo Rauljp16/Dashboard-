c366dfd387f2b72b8559f48468de3cbb9ebd24d8
Rauljp16 <raul_astra16@hotmail.com>
2024-06-24 01:55:16 +0200

    refactor: login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,41 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import fondo from "../images/fondo.jpg";
 
 function Login({ setAuth }) {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("rauljp16@gmail.com");
+  const [password, setPassword] = useState("hotel miranda");
   const navigate = useNavigate();
 
-  const loginStyle = {
+  const fondoStyle = {
+    backgroundImage: `url(${fondo})`,
+    backgroundSize: "cover",
     position: "absolute",
-    display: "flex",
-    gap: "10px",
     width: "100%",
-    height: "100%",
+    height: "100vh",
     left: "0",
     top: "0",
-    backgroundColor: "cadetblue",
-    flexDirection: "column",
+    display: "flex",
     justifyContent: "center",
     alignItems: "center",
   };
 
+  const loginStyle = {
+    display: "flex",
+    gap: "20px",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    boxShadow: "0px 0px 2px black",
+    borderRadius: "8px",
+    padding: "40px",
+    backgroundColor: "rgb(255 255 255 / 0.5)",
+    scale: "1.3",
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (email === "" && password === "") {
+    if (email === "rauljp16@gmail.com" && password === "hotel miranda") {
       setAuth("rauljp16");
       navigate("/dashboard");
     } else {
@@ -31,25 +44,27 @@ function Login({ setAuth }) {
   };
 
   return (
-    <form style={loginStyle} onSubmit={handleSubmit}>
-      <h1>LOGIN</h1>
-      <input
-        type="email"
-        placeholder="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit">LOGIN</button>
-      <p>To access...</p>
-      <p>EMAIL: rauljp16@gmail.com</p>
-      <p>PASSWORD: hotel miranda</p>
-    </form>
+    <div style={fondoStyle}>
+      <form style={loginStyle} onSubmit={handleSubmit}>
+        <h1>LOGIN</h1>
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">LOGIN</button>
+        <p>To access...</p>
+        <p>EMAIL: rauljp16@gmail.com</p>
+        <p>PASSWORD: hotel miranda</p>
+      </form>
+    </div>
   );
 }
 
