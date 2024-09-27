5707aa10825c697e9dc69090ea85900b4ccaa02e
Rauljp16 <raul_astra16@hotmail.com>
2024-09-27 09:17:43 +0200

    refactor: booking sin terminar

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -8,17 +8,17 @@ describe("Login Page", () => {
     cy.get('input[type="email"]').should("be.visible");
     cy.get('input[type="password"]').should("be.visible");
   });
-  // it("correct date email & password", () => {
-  //   cy.visit("http://localhost:5173/login");
-  //   cy.get('input[type="email"]').type("rauljp16@gmail.com");
-  //   cy.get('input[type="password"]').type("hotel miranda");
-  //   cy.get("btn-styled").click();
-  // });
-  // it("correct date email & password", () => {
-  //   cy.visit("http://localhost:5173/login");
-  //   cy.get('input[type="email"]').type("rauljp16@gmail.com");
-  //   cy.get('input[type="password"]').type("hotel miranda");
-  //   cy.get("btn-styled").click();
-  //   cy.url().should("include", "/dashboard");
-  // });
+  it("correct date email & password", () => {
+    cy.visit("http://localhost:5173/login");
+    cy.get('input[type="email"]').type("rauljp16@gmail.com");
+    cy.get('input[type="password"]').type("hotel miranda");
+    cy.get(".btn-styled").click();
+  });
+  it("correct date email & password", () => {
+    cy.visit("http://localhost:5173/login");
+    cy.get('input[type="email"]').type("rauljp16@gmail.com");
+    cy.get('input[type="password"]').type("hotel miranda");
+    cy.get(".btn-styled").click();
+    cy.url().should("include", "/dashboard");
+  });
 });
