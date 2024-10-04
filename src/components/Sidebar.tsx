cccb74febc5cc64b86b4373ddadec44c817959aa
Rauljp16 <raul_astra16@hotmail.com>
2024-10-04 15:41:39 +0200

    refactor: Sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -19,8 +19,8 @@ const SidebarStyled = styled.div<SidebarProps>`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: ${(props) => (props.open ? "280px" : "80px")};
-  min-width: ${(props) => (props.open ? "280px" : "80px")};
+  width: ${(props) => (props.open ? "270px" : "60px")};
+  min-width: ${(props) => (props.open ? "270px" : "60px")};
   transition: all 1s ease;
   overflow: hidden;
   z-index: 1;
@@ -40,25 +40,29 @@ const LogoWrapper = styled.div<SidebarProps>`
 const ImgStyled = styled.img`
   width: 100%;
 `;
-
+const DivLinks = styled.div`
+width: 100%;
+`;
 const LinkStyled = styled(Link) <LinkStyledProps>`
   position: relative;
   display: flex;
   align-items: center;
-  margin: 12px;
-  padding:${(props) => (props.isActive ? "2px 0px 2px 11px" : "2px 0px 2px 16px")};
+  /* margin: 12px; */
+  padding:${(props) => (props.open ? "10px 0px 10px 50px" : "10px 0px 10px 9px")};
   width: 100%;
   text-align: center;
   text-decoration: none;
-  border-left:${(props) => (props.isActive ? "4px solid red" : "")};
+  border-left:${(props) => (props.isActive ? "4px solid red" : "4px solid transparent")};
   color:${(props) => (props.isActive ? "red" : "#135846")};
-
+  transition: all 1s ease-in-out;
 `;
 
-const PStyled = styled.p<SidebarProps>`
+const PStyled = styled.div<SidebarProps>`
 position: absolute;
-left: 60px;
-transform: ${(props) => (props.open ? "translate(0) rotate(0deg)" : "translate(-120px) rotate(-90deg)")};
+left: 130px;
+font-size: 20px;
+font-weight: 600;
+transform: ${(props) => (props.open ? "translate(0) rotate(0deg)" : "translate(-250px) rotate(-90deg)")};
 transition: transform 1s ease-in-out;
 `;
 
@@ -70,31 +74,32 @@ function Sidebar({ open }: SidebarProps) {
       <LogoWrapper open={open}>
         <ImgStyled src={logo} alt="logo hotel" />
       </LogoWrapper>
+      <DivLinks>
+        <LinkStyled open={open} to="/dashboard" isActive={location.pathname === "/dashboard" || location.pathname === "/"}>
+          <RiDashboardLine style={{ width: "32px", height: "32px" }} />
+          <PStyled open={open}>Dashboard</PStyled>
+        </LinkStyled>
 
-      <LinkStyled open={open} to="/dashboard" isActive={location.pathname === "/dashboard" || location.pathname === "/"}>
-        <RiDashboardLine style={{ width: "28px", height: "28px" }} />
-        <PStyled open={open}>Dashboard</PStyled>
-      </LinkStyled>
+        <LinkStyled open={open} to="/bookings" isActive={location.pathname === "/bookings"}>
+          <FaRegCalendarAlt style={{ width: "32px", height: "32px" }} />
+          <PStyled open={open}>Bookings</PStyled>
+        </LinkStyled>
 
-      <LinkStyled open={open} to="/bookings" isActive={location.pathname === "/bookings"}>
-        <FaRegCalendarAlt style={{ width: "28px", height: "28px" }} />
-        <PStyled open={open}>Bookings</PStyled>
-      </LinkStyled>
+        <LinkStyled open={open} to="/rooms" isActive={location.pathname === "/rooms"}>
+          <IoKeyOutline style={{ width: "32px", height: "32px" }} />
+          <PStyled open={open}>Rooms</PStyled>
+        </LinkStyled>
 
-      <LinkStyled open={open} to="/rooms" isActive={location.pathname === "/rooms"}>
-        <IoKeyOutline style={{ width: "28px", height: "28px" }} />
-        <PStyled open={open}>Rooms</PStyled>
-      </LinkStyled>
+        <LinkStyled open={open} to="/contact" isActive={location.pathname === "/contact"}>
+          <MdOutlineContactPhone style={{ width: "32px", height: "32px" }} />
+          <PStyled open={open}>Contact</PStyled>
+        </LinkStyled>
 
-      <LinkStyled open={open} to="/contact" isActive={location.pathname === "/contact"}>
-        <MdOutlineContactPhone style={{ width: "28px", height: "28px" }} />
-        <PStyled open={open}>Contact</PStyled>
-      </LinkStyled>
-
-      <LinkStyled open={open} to="/users" isActive={location.pathname === "/users"}>
-        <HiOutlineUser style={{ width: "28px", height: "28px" }} />
-        <PStyled open={open}>Users</PStyled>
-      </LinkStyled>
+        <LinkStyled open={open} to="/users" isActive={location.pathname === "/users"}>
+          <HiOutlineUser style={{ width: "32px", height: "32px" }} />
+          <PStyled open={open}>Users</PStyled>
+        </LinkStyled>
+      </DivLinks>
     </SidebarStyled>
   );
 }
