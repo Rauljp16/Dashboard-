776b6da950a7fe7812015e6edb008f4758296e6a
Rauljp16 <raul_astra16@hotmail.com>
2024-10-14 17:14:48 +0200

    refactor: sidebar

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -6,7 +6,7 @@ function Dashboard() {
   const DashboardContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 30px;
+  gap: 29px;
   `;
   const SectionComments = styled.article`
   width: 100%;
